94cf376c637399e6391bbd8bb462019ff0e92fc6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-09-21 12:50:22 -0300

    Configuração do projeto, configuração das pastas e dos arquivos e instalação das libs que serão usadas

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from '../styles/HomeNoAuth.scss';
+
+const HomeNoAuth = () => {
+  return (
+    <>
+      <Head>
+          <title>Home</title>
+      </Head>
+      <main>
+
+      </main>
+    </>
+  );
+};
+
+export default HomeNoAuth;
