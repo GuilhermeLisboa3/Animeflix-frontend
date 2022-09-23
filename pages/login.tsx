fdcd5c93628011d2f617306e957284defff22db1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-09-23 15:41:26 -0300

    Criação do backend da parte de login, seu estilo visual e sua reponsividade

---
+++ pages/login.tsx
@@ -0,0 +1,111 @@
+import styles from "../styles/registerLogin.module.scss";
+import Head from "next/head";
+import { HeaderGeneric } from "../src/components/common/headerGeneric";
+import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
+import { Footer } from "../src/components/common/footer";
+import { useRouter } from "next/router";
+import { FormEvent, useEffect, useState } from "react";
+import { ToastComponet } from "../src/components/common/toast";
+import { authService } from "../src/services/authService";
+
+const Login = () => {
+  const router = useRouter();
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const [toastColor, setToastColor] = useState("");
+
+  useEffect(() => {
+    const registerSucess = router.query.registred;
+    if (registerSucess === "true") {
+      setToastColor("bg-success");
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+      setToastMessage("Cadastro feito com sucesso!");
+    }
+  }, [router.query]);
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+    const params = { email, password };
+
+    const {status} = await authService.login(params)
+    if(status === 200){
+        router.push("/home")
+    }else{
+        setToastColor("bg-danger");
+        setToastIsOpen(true);
+        setTimeout(() => {
+          setToastIsOpen(false);
+        }, 1000 * 3);
+        setToastMessage("Email ou senha incorretos!");
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Animeflix - Login</title>
+        <link rel="shortcut icon" href="/buttonPlay.svg" type="image/x-icon" />
+      </Head>
+      <main className={styles.main}>
+        <HeaderGeneric
+          logoUrl="/"
+          btnUrl="/register"
+          btnContent="Quero fazer parte"
+        />
+        <Container className="py-5">
+          <p className={styles.formTitle}>Bem-vindo(a) de volta!</p>
+          <Form className={styles.form} onSubmit={handleLogin}>
+            <p>
+              <strong>Bem-vindo(a) ao Animeflix</strong>
+            </p>
+            <FormGroup>
+              <Label for="email" className={styles.label}>
+                E-MAIL
+              </Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Qual o seu email?"
+                required
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password" className={styles.label}>
+                E-MAIL
+              </Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Qual a sua senha?"
+                minLength={6}
+                maxLength={20}
+                required
+                className={styles.input}
+              />
+            </FormGroup>
+            <Button outline className={styles.formBtn} type="submit">
+              ENTRAR
+            </Button>
+          </Form>
+          <ToastComponet
+            color={toastColor}
+            isOpen={toastIsOpen}
+            message={toastMessage}
+          />
+        </Container>
+        <Footer />
+      </main>
+    </>
+  );
+};
+
+export default Login;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -10,6 +10,11 @@ interface RegisterParams {
   password: string;
 }
 
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
 export const authService = {
   register: async (params: RegisterParams) => {
     const res = await api.post("/auth/register", params).catch((err) => {
@@ -17,6 +22,18 @@ export const authService = {
         return err.response;
       }
     });
+    return res;
+  },
+  login: async (params: LoginParams) => {
+    const res = await api.post("/auth/login", params).catch((err) => {
+      if (err.response.status == 400 || err.response.status === 401) {
+        return err.response;
+      }
+      return err
+    });
+    if(res.status === 200){
+      sessionStorage.setItem("animeflix-token", res.data.token)
+    }
     return res;
   },
 };
